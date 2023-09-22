f56dc362e3aa106f5e999855b8c4e9036e06ba30
hanpensen <hanpensen90@mail.ru>
2023-09-22 16:56:58 +0300

    add education_sys

--- education_system_api/serializers.py
+++ education_system_api/serializers.py
@@ -13,13 +13,3 @@ class LessonSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-# class ProductSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Product
-#         fields = '__all__'
-#
-#
-# class ViewSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = View
-#         fields = '__all__'
